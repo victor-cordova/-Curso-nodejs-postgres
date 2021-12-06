7eba89ad573893d76d7da02e044cc6d96ff562e7
victor_cordova <victorcordovapoma@gmail.com>
2021-12-06 11:52:59 -0500

    The lesson Manejando un Pool de conexiones wasn't, because of an experiment.

---
+++ config/config.js
@@ -0,0 +1,12 @@
+const config = {
+    env: process.env.NODE_ENV || "dev", //Indica en que entorno se está 
+    // desarrollo o produccion
+    port: process.env.PORT || 3000, //Se indica en que puerto está corriendo el proyecto.
+    dbUser: process.env.DB_USER,
+    dbPassword: process.env.DB_PASSWORD,
+    dbHost: process.env.DB_HOST,
+    dbName: process.env.DB_NAME,
+    dbPort: process.env.DB_PORT,
+}
+
+module.exports = { config };
--- libs/postgres.pool.js
+++ libs/postgres.pool.js
@@ -1,5 +1,10 @@
 const { Pool } = require("pg");
 
+const config = require("../config/config"); //Por medio de estas variables de 
+//entorno se evita colocar la data del cliente como se muestra abajo.
+
+const USER = encodeURIComponent(); //Se va a proteger el 
+//usuario antes de enviarlo como URI
 
 const pool = new Pool ({  //Al momento de crear un nuevo cliente implementará
     // un await interno para compartir la conexión con las nuevas peticiones.
